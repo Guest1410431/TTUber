c7cc4c53d42c694458347dad10035ba27c085e86
Michael Lieb <mal.lieb121@gmail.com>
2018-04-03 10:25:08 -0500

    Initial commit

---
+++ TTUber/app/src/main/java/ttuber/com/ttuber/RegisterActivity.java
@@ -0,0 +1,13 @@
+package ttuber.com.ttuber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
